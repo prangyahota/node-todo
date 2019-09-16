9958352e113a123e9a320be9dd9ba15439a8591b
rohitiacr203 <rohitiacr203@gmail.com>
2019-09-16 15:22:14 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true/simdb',
+    remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
