17b080442ebfc2c454342ef7e7d9e1fe64276413
rohitiacr203 <rohitiacr203@gmail.com>
2019-09-24 13:25:44 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true',
+    //remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true',
    // remoteUrl : 'mongodb://rndsimulator:OXmp9DM67vmKToAiOJHuocUcudFZ5ugdx3YnywP296RAAvY1ulTuievpnRDFJTqzgXBgHVamLT9VXVuDcRU1kg==@rndsimulator.documents.azure.com:10255/?ssl=true',
-   // remoteUrl : 'mongodb://simulatornd:DfckcFbf5V3Jl0DjJACprsgrcOoXlH0JEbLu1owFVcDIXaVGOh6msYLK7mAxfWpcKPKstEJ5jwpj5hOYvwF7pQ==@simulatornd.documents.azure.com:10255/?ssl=true'
+    remoteUrl : 'mongodb://simulatornd:DfckcFbf5V3Jl0DjJACprsgrcOoXlH0JEbLu1owFVcDIXaVGOh6msYLK7mAxfWpcKPKstEJ5jwpj5hOYvwF7pQ==@simulatornd.documents.azure.com:10255/?ssl=true'
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
