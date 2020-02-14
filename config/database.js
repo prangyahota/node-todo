6d44fe865308685456dcbb732e6d0649a0a2bed7
prangyahota <prangya.hota11@gmail.com>
2020-02-14 16:02:01 +0530

    updated database.js
    updated for my own app service

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true',
-    remoteUrl : 'mongodb://simulatornd:DfckcFbf5V3Jl0DjJACprsgrcOoXlH0JEbLu1owFVcDIXaVGOh6msYLK7mAxfWpcKPKstEJ5jwpj5hOYvwF7pQ==@simulatornd.documents.azure.com:10255/?ssl=true'
+    remoteUrl : 'mongodb://angularcosmos-database:80rrQXVG0hzHcglUvBSo7wa1hAD8mOXtdYkCE5H1On5PxwXsQN3x4d3pdzInJfxIS5XDIpBfNH0Bk2wS7ZdHRg==@angularcosmos-database.documents.azure.com:10255/?ssl=true'
    // localUrl: 'mongodb://localhost/meanstacktutorials'
 };
