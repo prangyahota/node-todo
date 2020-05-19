2e3ec0168d60afdde3e4f62d37863703de510f49
prangyahota <prangya.hota11@gmail.com>
2020-05-19 19:35:26 +0530

    updated for cosmoslocalhost

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     //remoteUrl : 'mongodb://simulatordb:NLrWC26b9rqTvmQSEM0CjwIP5ilQRa8zzYcKnZGTQmvWoUHzJOiAk15BWsfF8geVpal2lh0WEeu4gMWwDyCLcg==@simulatordb.documents.azure.com:10255/?ssl=true',
-    remoteUrl : 'mongodb://angularcosmos-database:80rrQXVG0hzHcglUvBSo7wa1hAD8mOXtdYkCE5H1On5PxwXsQN3x4d3pdzInJfxIS5XDIpBfNH0Bk2wS7ZdHRg==@angularcosmos-database.documents.azure.com:10255/?ssl=true'
+    remoteUrl :'mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true'
+    //'mongodb://angularcosmos-database:80rrQXVG0hzHcglUvBSo7wa1hAD8mOXtdYkCE5H1On5PxwXsQN3x4d3pdzInJfxIS5XDIpBfNH0Bk2wS7ZdHRg==@angularcosmos-database.documents.azure.com:10255/?ssl=true'
    // localUrl: 'mongodb://localhost/meanstacktutorials'
 };
